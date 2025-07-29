2fd5ce6cbc96001f70adf9029de3f588d00a77c0
Roberto Júnior <rbezerra18@hotmail.com>
2025-07-29 19:06:07 -0300

    Curso Python Edutin Academy Actividad 1. Operaciones

---
+++ actividad1_operaciones.py
@@ -0,0 +1,16 @@
+#Pedir números al usuario
+primer_numero = int(input("Ingresa un número (1): "))
+segundo_numero = int(input("Ingresa un número (2): "))
+
+#Operaciones aritméticas
+
+#Suma
+print(f'La suma es {primer_numero + segundo_numero}')
+
+#Multiplicación
+print(f'La multiplicación es {primer_numero * segundo_numero}')
+
+#Operaciones de comparación
+print(f'¿Los números son iguales? {primer_numero == segundo_numero}')
+print(f'¿El primer número es menor que el segundo? {primer_numero < segundo_numero}')
+print(f'¿El segundo número es mayor o igual al primero? {segundo_numero >= primer_numero}')
